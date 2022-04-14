a74590f63f3184797ede36d93ebd96ed3e51ffc6
eason long <46415452+longxianlei@users.noreply.github.com>
2022-04-14 11:23:33 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,9 +12,11 @@ using namespace std;
 
 int main()
 {
+	// The speed range for pan-tilt is:
+	// 0-63 for pan speed. i.e., 0x00-0x3F, when using turbo (0xFF), the pan motor will working in maximum speed.
+	// 0-63 for tilt speed.
 	
 	CameraAction camera1;
-	
 	// Open camera and connect the usb port.
 	// The com port is COM4. You can change it decided.
 	if (!camera1.openCamera(4))
@@ -24,10 +26,6 @@ int main()
 	}
 	BYTE pByte[8] = { 0 };
 
-	
-	// The speed range for pan-tilt is:
-	// 0-63 for pan speed. i.e., 0x00-0x3F, when using turbo (0xFF), the pan motor will working in maximum speed.
-	// 0-63 for tilt speed.
 	cout << "to left: " << endl;
 	camera1.toLeft(pByte, 0xFF);
 	for (int i = 0; i < PELCO_P_SIZE; i++)
@@ -37,7 +35,6 @@ int main()
 	cout << endl;
 	camera1.sendCommand(pByte);
 	this_thread::sleep_for(chrono::milliseconds(2000));
-
 
 	cout << "to down:" << endl;
 	camera1.toDown(pByte, 0xFF);
@@ -49,7 +46,6 @@ int main()
 	camera1.sendCommand(pByte);
 	this_thread::sleep_for(chrono::milliseconds(2000));
 
-
 	cout << "stop!!!" << endl;
 	camera1.stop(pByte);
 	for (int i = 0; i < PELCO_P_SIZE; i++)
@@ -59,7 +55,6 @@ int main()
 	cout << endl;
 	camera1.sendCommand(pByte);
 	this_thread::sleep_for(chrono::milliseconds(2000));
-
 	
 	cout << "up!!!" << endl;
 	camera1.toUp(pByte,0xFF);
@@ -71,7 +66,6 @@ int main()
 	camera1.sendCommand(pByte);
 	this_thread::sleep_for(chrono::milliseconds(2000));
 
-
 	cout << "to right!!!" << endl;
 	camera1.toRight(pByte, int(80));
 	for (int i = 0; i < PELCO_P_SIZE; i++)
@@ -82,7 +76,6 @@ int main()
 	camera1.sendCommand(pByte);
 	this_thread::sleep_for(chrono::milliseconds(2000));
 
-
 	cout << "to left up!!!" << endl;
 	camera1.toLeftUp(pByte, int(80), 25);
 	for (int i = 0; i < PELCO_P_SIZE; i++)
@@ -92,7 +85,6 @@ int main()
 	cout << endl;
 	camera1.sendCommand(pByte);
 	this_thread::sleep_for(chrono::milliseconds(5000));
-
 
 	cout << "to right down!!!" << endl;
 	camera1.toRightDown(pByte, 44, 25);
